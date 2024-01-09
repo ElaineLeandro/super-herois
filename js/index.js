97a72022b09f766ea32f214bfac2b6fbe4942fc7
ThiagoSS1 <thiago_seta@hotmail.com>
2024-01-09 19:29:07 -0300

    feat: adicionando evento no botao trailer

---
+++ js/index.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+  let btn = document.querySelector('#btn-navbar');
+  console.log("caiu aqui", btn);
+  btn.addEventListener('click', function () {
+    let confirm = window.confirm('Deseja sair dessa pagina?');
+    if (confirm) {
+      window.location.href = 'https://www.youtube.com/watch?v=9fVYKsEmuRo&ab_channel=PlayStation';
+    }
+  });
+}
