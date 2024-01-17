5c736a7ae6aa668c2b9e24982f0915ca52a5b38c
ElaineLeandro <elainerleandro1@gmail.com>
2024-01-17 20:10:21 -0300

    feat: commics-search adção api

---
+++ js/api.js
@@ -0,0 +1,20 @@
+// api.js
+// import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: 'https://akabab.github.io/superhero-api/api',
+});
+
+export function fetchSuperheroes(){
+    return instance.get('/all.json')
+    .then(response => {
+        console.log(response.data);
+        return response.data;
+    })
+    .catch(error => {
+        console.error('Erro ao obter dados da API:', error);
+        throw error;
+    })
+
+    
+}
--- js/comics-search.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('btn-loading').addEventListener('click', function () {
-    alert('Aguarde por favor, enquanto o conteúdo está sendo carregado.');
-    
-});
